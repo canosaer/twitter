2f3b927ba46b04ed76e14bdaa331a59b8888538b
canosaer <canosaer@gmail.com>
2021-05-01 14:22:55 -0400

    almost done

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -27,23 +27,25 @@ class TwitterAPI {
 
     handleSearch = (evt) => {
         const query = document.querySelector(`.search-controls__input`).value
-        
-        let selectedSort = `mixed`
-        if(this.sortText.textContent.indexOf(`Popularity`) !== -1) selectedSort = `popular`
-        else if(this.sortText.textContent.indexOf(`Newest`) !== -1) selectedSort = `recent`
+        if(query){
+            let selectedSort = `mixed`
+            if(this.sortText.textContent.indexOf(`Popularity`) !== -1) selectedSort = `popular`
+            else if(this.sortText.textContent.indexOf(`Newest`) !== -1) selectedSort = `recent`
 
-        const numResults = parseInt(this.countText.textContent.substring(0,3).trim())
-        
+            const numResults = parseInt(this.countText.textContent.substring(0,3).trim())
+            
 
-        const data = {
-            op: `search_tweets`,
-            q: query,
-            result_type: selectedSort,
-            count: numResults,
+            const data = {
+                op: `search_tweets`,
+                q: query,
+                result_type: selectedSort,
+                count: numResults,
+            }
+
+
+            axios.get(this.API_BASE_URL, { params: data }).then(this.processResults).catch(this.handleError)
         }
-
-
-        axios.get(this.API_BASE_URL, { params: data }).then(this.processResults).catch(this.handleError)
+        
     }
 
     processResults = (data) => {
@@ -77,13 +79,68 @@ class TwitterAPI {
 
             let atRowP = document.createElement(`p`)
             atRowP.classList.add(`results__at-row`)
-
+            atRowP.textContent = `@`
             
+            let screenNameSpan = document.createElement(`span`)
+            screenNameSpan.classList.add(`results__screen-name`)
+            screenNameSpan.textContent = tweet.user.screen_name
+            atRowP.appendChild(screenNameSpan)
 
+            resultRow.appendChild(atRowP)
+
+            let tweetP = document.createElement(`p`)
+            tweetP.classList.add(`results__tweet`)
+            let rawTweet = tweet.text
+            let linkIndex = rawTweet.indexOf(`https://`)
+            if(linkIndex !== -1){
+                let tweetAnchor = document.createElement(`a`)
+                tweetAnchor.href = rawTweet.substring(linkIndex, rawTweet.length)
+                tweetAnchor.setAttribute(`target`, `_blank`)
+                tweetAnchor.textContent = rawTweet.substring(linkIndex, rawTweet.length)
+                tweetAnchor.style.color = `skyblue`
+                tweetP.textContent = rawTweet.substring(0,linkIndex) + ` `
+                tweetP.appendChild(tweetAnchor)
+            }
+            else tweetP.textContent = rawTweet
+            resultRow.appendChild(tweetP)
+
+            let timestampP = document.createElement(`p`)
+            timestampP.classList.add(`results__timestamp`)
+            timestampP.textContent = this.formatTimestamp(tweet.created_at)
+            resultRow.appendChild(timestampP)
+
+            resultsSection.appendChild(resultRow)
         });
 
         
 
+    }
+
+    formatTimestamp = (rawTimestamp) => {
+        let month = ``
+        let inputMonth = rawTimestamp.substring(4,7)
+
+        if(inputMonth === `Jan`) month = `1`
+        else if(inputMonth === `Feb`) month = `2`
+        else if(inputMonth === `Mar`) month = `3`
+        else if(inputMonth === `Apr`) month = `4`
+        else if(inputMonth === `May`) month = `5`
+        else if(inputMonth === `Jun`) month = `6`
+        else if(inputMonth === `Jul`) month = `7`
+        else if(inputMonth === `Aug`) month = `8`
+        else if(inputMonth === `Sep`) month = `9`
+        else if(inputMonth === `Oct`) month = `10`
+        else if(inputMonth === `Nov`) month = `11`
+        else if(inputMonth === `Dec`) month = `12`
+
+        let day = ``
+        let rawDay = rawTimestamp.substring(8,10)
+        if(rawDay[0] === `0`) day = rawDay[1]
+        else day = rawDay
+
+        let year = rawTimestamp.substring(28,30)
+        
+        return `${month}/${day}/${year}`
     }
 
     handleError = (evt) => {
